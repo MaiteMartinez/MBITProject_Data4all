4240439e64b6e9f5c794286ea62c808e98d31d55
MaiteMartinez <31801159+MaiteMartinez@users.noreply.github.com>
2017-10-06 20:51:17 +0200

    Delete twitter_keys.py

--- Python/twitter_keys.py
+++
@@ -1,6 +0,0 @@
-{"Maite":{	"consumer_key" : "jAr5xF7mMjYMNLxSB2jaLRgtD",
-			"consumer_secret" : "V2VnFz1AqMBq1BrwofociLwiF4wA5LcZKqohgJ1IhQ93679Nta",
-			"access_token" : "906950076123185152-4NoTPkFaDBTB5GiipLLbWJ42SWRLzEJ",
-			"access_token_secret" : "dqD1Tw9r0PBwaI2tkfwSvsqImTyBiKEBoTW5I7XtWcQik"
-		}
-}
